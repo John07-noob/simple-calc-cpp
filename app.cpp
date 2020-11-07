b32ec92fc15ad023d9fd6da7b8a9e8148b9d0e03
John Stone <65850328+John07-noob@users.noreply.github.com>
2020-11-08 07:14:06 +0800

    Add files via upload

---
+++ app.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+int main() {
+    std::string user_input;
+    int user_num1, user_num2, result;
+
+while(true){
+    std::cout << "Welcome To Simple Calc\n";
+    std::cout << "1.Addition\n";
+    std::cout << "2.Subtraction\n";
+    std::cout << "3.Multiplication\n";
+    std::cout << "4.Division\n";
+    std::cout << "Choose The Operation: ";
+    
+    std::cin >> user_input;
+    if (user_input == "1") {
+        std::cout << "Insert Number Here: ";
+        std::cin >> user_num1;
+        if (std::cin.fail()) {
+            std::cout << "Invalid Command!\n";
+        }else{
+            std::cout << "Insert Number Here: ";
+            std::cin >> user_num2;
+            if (std::cin.fail()) {
+                std::cout << "Invalid Command!\n";
+            }else {
+                result = user_num1 + user_num2;
+                printf("The Result: %d\n", result);
+            }
+        }
+
+    } else if (user_input == "2") {
+        std::cout << "Insert Number Here: ";
+        std::cin >> user_num1;
+        if (std::cin.fail()) {
+            std::cout << "Invalid Command!\n";
+        }else {
+            std::cout << "Insert Number Here: ";
+            std::cin >> user_num2;
+            if (std::cin.fail()) {
+                std::cout << "Invalid Command!\n";
+            }else {
+                result = user_num1 - user_num2;
+                printf("The Result: %d\n", result);
+            }
+        }
+
+    } else if (user_input == "3") {
+        std::cout << "Insert Number Here: ";
+        std::cin >> user_num1;
+        if (std::cin.fail()) {
+            std::cout << "Invalid Command!\n";
+        }else {
+            std::cout << "Insert Number Here: ";
+            std::cin >> user_num2;
+            if (std::cin.fail()) {
+                std::cout << "Invalid Command!\n";
+            }else {
+                result = user_num1 * user_num2;
+                printf("The Result: %d\n", result);
+            }
+        }
+
+    } else if (user_input == "4") {
+        std::cout << "Insert Number Here: ";
+        std::cin >> user_num1;
+        if (std::cin.fail()) {
+            std::cout << "Invalid Command!\n";
+        }else {
+            std::cout << "Insert Number Here: ";
+            std::cin >> user_num2;
+            if (std::cin.fail()) {
+                std::cout << "Invalid Command!\n";
+            }else {
+                result = user_num1 / user_num2;
+                printf("The Result: %d\n", result);
+            }
+        }
+    } else {
+        std::cout << "Invalid Command!\n";
+    }
+    
+}
+}
